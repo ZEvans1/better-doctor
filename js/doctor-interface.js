4331274f367fb3794753bdef03e0c0edfbd92873
ZEvans1 <zte.zachary@gmail.com>
2018-02-16 10:41:43 -0800

    Add code for an additional API call for a doctor's name.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,8 +1,10 @@
 import { promise } from './../js/doctor.js';
+import { promiseTwo } from './../js/doctor-name.js';
 import apiKey from './../.env';
 
 $(document).ready(function() {
   let condition;
+  let lastName;
 
   $('#condition-form').submit(function(event) {
     event.preventDefault();
@@ -15,6 +17,23 @@ $(document).ready(function() {
       let body = JSON.parse(response);
         for (let i = 0; i < body.data.length; i++) {
           $('#result').append(body.data[i].profile.first_name + " " + body.data[i].profile.last_name + ", " + body.data[i].profile.title + "<br>");
+        }
+    }, function(error) {
+      $('.showErrors').text(`There was an error: $(error.message)`);
+    });
+  });
+
+  $('#doctor-form').submit(function(event) {
+    event.preventDefault();
+    let lastName = $('#last-name').val();
+    let newSearch = promiseTwo(lastName);
+    $('#doctor-info').empty();
+
+    newSearch.then(function(response) {
+
+      let body = JSON.parse(response);
+        for (let i = 0; i < body.data.length; i++) {
+          $('#doctor-info').append(body.data[i].profile.last_name + ", " + body.data[i].profile.first_name + ", " + body.data[i].profile.title + "<br>");
         }
     }, function(error) {
       $('.showErrors').text(`There was an error: $(error.message)`);
