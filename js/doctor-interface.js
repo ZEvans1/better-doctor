0a22155c8504f2f4f38b51e422bc65ba56a04c53
ZEvans1 <zte.zachary@gmail.com>
2018-02-16 14:05:19 -0800

    Add code to include more doctor information and handle search terms that yield no results.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -15,6 +15,9 @@ $(document).ready(function() {
     newSearch.then(function(response) {
 
       let body = JSON.parse(response);
+      if (body.data.length < 1) {
+        $('#result').text("No doctors found for your search term. Please try another term.")
+      }
         for (let i = 0; i < body.data.length; i++) {
           $('#result').append(body.data[i].profile.first_name + " " + body.data[i].profile.last_name + ", " + body.data[i].profile.title + "<br>");
         }
@@ -32,9 +35,18 @@ $(document).ready(function() {
     newSearch.then(function(response) {
 
       let body = JSON.parse(response);
+      if (body.data.length < 1) {
+        $('#doctor-info').text("No doctors found for your search term. Please make sure you entered the last name you saw above correctly.")
+      }
         for (let i = 0; i < body.data.length; i++) {
+          let acceptsPatients;
+          if (body.data[i].practices[0].accepts_new_patients == true) {
+            acceptsPatients = "yes";
+          } else {
+            acceptsPatients = "no";
+          }
           $('#doctor-info').append(body.data[i].profile.last_name + ", " + body.data[i].profile.first_name + ", " + body.data[i].profile.title + "<br>" + '<img src =' + body.data[i].profile.image_url + '>' + "<br>");
-          $('#doctor-info').append("Visiting address: " + body.data[i].practices[0].visit_address.street + ", " + body.data[i].practices[0].visit_address.state + ", " + body.data[i].practices[0].visit_address.zip + "<br>");
+          $('#doctor-info').append("Visiting address: " + body.data[i].practices[0].visit_address.street + ", " + body.data[i].practices[0].visit_address.state + ", " + body.data[i].practices[0].visit_address.zip + "<br>" + "Accepting new patients: " + acceptsPatients + "<br>" + "<br>");
           for (let j = 0; j < body.data[i].practices[0].phones.length; j++) {
             $('#doctor-info').append("Phone #: " + body.data[i].practices[0].phones[j].number + "<br>" + "Type : " + body.data[i].practices[0].phones[j].type + "<br>" + "<br>");
           }
